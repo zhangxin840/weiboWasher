5804141848cb3bd5e7a4b98787de75f5f79d44cd
Zhang Xin <zhangxin840@gmail.com>
2013-06-30 20:34:10 +0800

    Release 1.3.4

---
+++ README.md
@@ -0,0 +1,128 @@
+微博洗刷刷 
+===========
+
+![alt text][logo]
+
+
+####清理微博中的广告
+
+![alt text][inline ad]
+
+####隐藏不想要的栏目
+
+![alt text][contents]
+
+####清理提示
+
+![alt text][notifications]
+
+
+####不改变微博原貌
+
+
+####清理规则实时更新
+
+
+####方便，开源，安全
+
+
+
+
+[Chrome Web Store 官方商店](https://chrome.google.com/webstore/detail/%E5%BE%AE%E5%8D%9A%E6%B4%97%E5%88%B7%E5%88%B7/pboegggfndemleaecklchlhmaaacbpoo)
+-----------
+
+* 推荐的安装方式，可自动升级 (需要谷歌ID)
+
+* 支持Chromium内核浏览器，双核浏览器
+
+* 例如：![alt text][browser chrome]Chrome浏览器，![alt text][browser 360]360浏览器，![alt text][browser jisu]360极速浏览器，![alt text][browser liebao]猎豹浏览器
+
+
+
+[在线安装](https://github.com/zhangxin840/weiboWasher/raw/master/plugin.crx)
+------------
+
+* ![alt text][browser 360]360浏览器，![alt text][browser jisu]360极速浏览器，![alt text][browser liebao]猎豹浏览器
+
+  * 点此[**一键安装**](https://github.com/zhangxin840/weiboWasher/raw/master/plugin.crx)
+
+
+拖拽安装
+------------
+
+* ![alt text][browser chrome]Chrome浏览器：
+
+  * [右键另存此链接](https://github.com/zhangxin840/weiboWasher/raw/master/plugin.crx)
+
+  * 点击 **右上角菜单按钮** 进入 **工具--扩展程序** 页面 (或在地址栏输入chrome://extensions/直接进入)
+  
+  * 将另存的插件文件拖入 **工具--扩展程序** 页面中 (拖入这个页面才能顺利安装，其他页面不行)
+  
+  * 如图所示：![alt text][drag install]
+
+
+
+使用
+------------
+
+![alt text][usage]
+
+
+
+参与开发
+----------
+
+data / settings.json 为实时更新的配置文件
+
+开发者可修改settings.json来更正过滤规则
+
+[Chrome扩展开发手册](https://developer.chrome.com/extensions/getstarted.html)
+
+
+
+作者
+----------
+
+插件制作：[@梦见新中国-张鑫](http://weibo.com/mjxzg)
+
+原画：[@我是大巧](http://weibo.com/u/1785489211)
+
+平面设计：Simon
+
+内测： Wang Zhe, Han Yinglong, Jiao Wei, Mei Xiaomeng
+
+作者邮箱: zhangxin840@gmail.com
+
+QQ: 116943915
+
+
+[微博分享](http://v.t.sina.com.cn/share/share.php?&url=http://zhangxin840.github.io/weiboWasher/&title=微博洗刷刷： 去除微博中的广告，屏蔽自己不感兴趣的内容。小巧方便，无需繁琐设置，不改变微博原貌。开源插件，安全可靠。&pic=https://raw.github.com/zhangxin840/weiboWasher/master/img/weiboWasher-300.png&ralateUid=1955199891)
+----------
+
+
+支持
+----------
+
+[捐助作者](https://me.alipay.com/zhangxin840)
+
+
+
+[logo]: https://raw.github.com/zhangxin840/weiboWasher/master/img/weiboWasher-300.png "微博洗刷刷"
+
+[inline ad]: https://raw.github.com/zhangxin840/weiboWasher/master/img/inlineAds.jpg "微博中的广告"
+
+[contents]: https://raw.github.com/zhangxin840/weiboWasher/master/img/contents.jpg "不想要的栏目"
+
+[notifications]: https://raw.github.com/zhangxin840/weiboWasher/master/img/notifications.jpg "恼人的提示"
+
+[drag install]: https://raw.github.com/zhangxin840/weiboWasher/master/img/drag-install.jpg "拖拽安装"
+
+[browser chrome]: https://raw.github.com/zhangxin840/weiboWasher/master/img/browser-small-chrome.png "支持Chrome浏览器"
+
+[browser 360]: https://raw.github.com/zhangxin840/weiboWasher/master/img/browser-small-360.jpg "支持360浏览器"
+
+[browser jisu]: https://raw.github.com/zhangxin840/weiboWasher/master/img/browser-small-jisu.jpg "支持360极速浏览器"
+
+[browser liebao]: https://raw.github.com/zhangxin840/weiboWasher/master/img/browser-small-leibao.jpg "支持猎豹浏览器"
+
+[usage]: https://raw.github.com/zhangxin840/weiboWasher/master/img/popup.jpg "打开菜单"
---
+++ plugin/popup.js
@@ -0,0 +1,136 @@
+var storagePerfix = "options_";
+
+// Sample data
+var checkOptions = [{
+	name : "recommend",
+	displayName : "推荐内容",
+	checked : true,
+}, {
+	name : "related",
+	displayName : "相关搜索",
+	checked : false,
+}];
+
+var generateOptions = function() {
+	var newOption;
+	var index;
+	var option;
+	var options = checkOptions;
+
+	for (index in options) {
+		option = options[index];
+
+		if (!option.name || ( typeof option.checked === "undefined") || !option.displayName) {
+			console.log("Option data error");
+		} else {
+			newOption = $("#options .js-optionContainer.js-template").clone().removeClass("js-template");
+			newOption.find("label").attr("for", option.name);
+			newOption.find("label").html(option.displayName);
+			if (option.checked) {
+				newOption.find("input").prop('checked', true);
+			} else {
+				newOption.find("input").prop('checked', false);
+			}
+			newOption.find("input").attr("name", option.name);
+			$("#options").append(newOption);
+		}
+	}
+};
+
+var getLoaclOptions = function() {
+	var reg = new RegExp('^' + storagePerfix);
+	var storage = localStorage;
+	var options = [];
+	var option;
+	var name;
+	var innerIndex;
+
+	if (!storage) {
+		throw "localStorage not available.";
+	}
+
+	for (key in localStorage) {
+		if (reg.test(key)) {
+			if (key.indexOf("_displayName") < 0) {
+				option = {};
+				name = key.substring(key.indexOf("_") + 1);
+
+				option["name"] = name;
+				if (storage[key] === "true") {
+					option["checked"] = true;
+				} else if (storage[key] === "false") {
+					option["checked"] = false;
+				}
+
+				options.push(option);
+			}
+
+		}
+	}
+
+	for (key in localStorage) {
+		if (reg.test(key) && key.indexOf("_displayName") >= 0) {
+			name = key.substring(key.indexOf("_") + 1, key.lastIndexOf("_"))
+			for (innerIndex in options) {
+				if (options[innerIndex].name === name) {
+					options[innerIndex].displayName = localStorage[key];
+				}
+			}
+
+		}
+	}
+
+	checkOptions = options;
+};
+
+var setupOptions = function() {
+	var $options = $(".js-optionContainer").not(".js-template");
+
+	$("#options").on("click", "input", optionCheckChanged);
+};
+
+var optionCheckChanged = function() {
+	var storage = localStorage;
+	var fullName = storagePerfix + $(this).attr("name");
+
+	if (!storage) {
+		throw "localStorage not available.";
+	}
+
+	if ($(this).prop('checked')) {
+		storage[fullName] = "true";
+
+	} else {
+		storage[fullName] = "false";
+	}
+};
+
+getLoaclOptions();
+generateOptions();
+setupOptions();
+
+// Google Analyse
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-41995758-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = 'https://ssl.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
+})();
+
+// Track UI
+function trackUi(e) {
+    _gaq.push(['_trackEvent', e.target.getAttribute("name"), 'clicked']);
+};
+
+(function() {
+	$("[name]").click(trackUi);
+})();
+
+
+
